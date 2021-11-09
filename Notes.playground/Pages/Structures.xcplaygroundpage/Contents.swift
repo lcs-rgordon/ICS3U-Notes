7cd220af201cc87b3ed703011acbfba3f33dbde0
Russell Gordon <rgordon@lcs.on.ca>
2021-11-09 09:08:26 -0500

    Updated comments.

--- Notes.playground/Pages/Structures.xcplaygroundpage/Contents.swift
+++ Notes.playground/Pages/Structures.xcplaygroundpage/Contents.swift
@@ -3,6 +3,7 @@ import Cocoa
 // Define a structure to describe a Person
 // Like a blueprint, or a template...
 struct Person {
+    
     // MARK: Stored Properties
     // Each property is an attribute of a given person
     var name: String
@@ -19,19 +20,22 @@ struct Person {
 }
 
 // Make an instance of Person, named "me"
+// Parameters are name, hairColor, age, et cetera
+// Arguments are "Mr. Gordon", "red", 44, et cetera
 var me = Person(name: "Mr. Gordon",
                 hairColor: "red",
                 age: 44,
                 heightInCentimetres: 180.0,
                 massInKilograms: 79.9)
 
+// Accessing stored properties
 me.name
 me.hairColor
 me.age
 me.heightInCentimetres
+
+// Accessing a computed property
 me.heightInMetres
-me.age = 45
-me.age
 
 
 
